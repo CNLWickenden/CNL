5b43d1e98c0ae1e0e55aac6ea8d028fe723c9cef
Chippy <sdk58@case.edu>
2012-11-03 20:28:11 -0400

    Uploaded Initial Shell for Epochs Class

---
+++ TimeSeries/cnl_Epochs.m
@@ -0,0 +1,24 @@
+classdef cnl_Epochs < handle
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        epochs %Nx2 matrix, where n is the number of epochs
+    end
+    
+    methods
+        function obj = cnl_Epochs(timeWindows)
+        end
+        
+        %construct epochs from a series of times
+        
+        %intersect epochs
+        
+        %union epochs
+        
+        
+        
+    end
+    
+end
+
